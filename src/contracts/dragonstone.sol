1041cc3a5550e3b76b76277be76c873ceadc0437
Niloo Ravaei <nilooravaei@gmail.com>
2018-09-06 13:39:14 -0400

    update dragonstone contract to v0.4.24 solidity

--- src/contracts/dragonstone.sol
+++ src/contracts/dragonstone.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.24;
 
 /*
 Currency that can only be issued by its creator and transferred to anyone
@@ -10,19 +10,19 @@ contract DragonStone {
     // event that notifies when a transfer has completed
     event Delivered(address from, address to, uint amount);
 
-    function DragonStone() {
+    constructor() public {
         creator = msg.sender;
     }
 
-    function create(address receiver, uint amount) {
+    function create(address receiver, uint amount) public {
         if (msg.sender != creator) return;
         balances[receiver] += amount;
     }
 
-    function transfer(address receiver, uint amount) {
+    function transfer(address receiver, uint amount) public {
         if (balances[msg.sender] < amount) return;
         balances[msg.sender] -= amount;
         balances[receiver] += amount;
-        Delivered(msg.sender, receiver, amount);
+        emit Delivered(msg.sender, receiver, amount);
     }
 }
